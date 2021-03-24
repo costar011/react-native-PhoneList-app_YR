c853a7a08bfc4eb86750129fabbf42f351ec5290
YERIM <dnaer456@gmail.com>
2021-03-24 21:11:16 +0900

    list add

--- App.js
+++ App.js
@@ -1,11 +1,43 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import {
+  StatusBar,
+  SafeAreaView,
+  View,
+  Text,
+  StyleSheet,
+  FlatList,
+} from "react-native";
 
 const phonelist = [
   {
     id: `phone1`,
-    name: ``,
-    price: ``,
+    name: `iPhone 11`,
+    price: `3000000`,
+  },
+  {
+    id: `phone2`,
+    name: `12 pro`,
+    price: `3500000`,
+  },
+  {
+    id: `phone3`,
+    name: `갤럭시 s10`,
+    price: `1300000`,
+  },
+  {
+    id: `phone4`,
+    name: `갤럭시 s21`,
+    price: `3200000`,
+  },
+  {
+    id: `phone5`,
+    name: `iPhone se`,
+    price: `2300000`,
+  },
+  {
+    id: `phone6`,
+    name: `갤럭시 s20`,
+    price: `1600000`,
   },
 ];
 
